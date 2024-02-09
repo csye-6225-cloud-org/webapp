29409b161c6104acfe9a7e834b5edf488b111067
Anuraag Bathula <bathula.anuraag.100@gmail.com>
2024-02-08 20:50:27 -0500

    Finished update and get user features with appropriate auth

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -8,8 +8,8 @@ exports.createuser =  async (req, res) => {
     res.set('Content-Type', 'application/json');
     res.set('X-Content-Type-Options', 'nosniff');
 
-    if( Object.keys(req.query).length > 0 || req.body.first_name == "" || req.body.first_name == null || req.body.last_name == "" || req.body.last_name == null 
-    || req.body.password == "" || req.body.password == null || req.body.username == "" || req.body.username == null ){
+    if( Object.keys(req.query).length > 0 || req.body.first_name == "" || req.body.first_name == null || req.body.first_name == undefined || req.body.last_name == "" || req.body.last_name == null || req.body.last_name == undefined 
+    || req.body.password == "" || req.body.password == null || req.body.password == undefined || req.body.username == "" || req.body.username == null || req.body.username == undefined ){
         res.status(400).end();
     }
 
@@ -29,7 +29,6 @@ exports.createuser =  async (req, res) => {
         
             User.create(user)
                 .then(data => {
-                    delete data.password;
                     dataToSend = {
                         id: data.id,
                         first_name: data.first_name,
@@ -50,8 +49,11 @@ exports.createuser =  async (req, res) => {
             // Some task on success
         },
         (onRejected) => {
-            console.log("Error with user creation: "+ onRejected);
+            console.log("Error with password hashing: "+ onRejected);
             // Some task on failure
+            res.status(500).send({
+                message: onRejected
+              });
         }
     )}
 
@@ -60,35 +62,143 @@ exports.getuser =  async (req, res) => {
     res.set('Content-Type', 'application/json');
     res.set('X-Content-Type-Options', 'nosniff');
 
-    console.log("Auth done, inside getuser");
-    res.status(200).end();
+    // console.log("Auth done, inside getuser");
+    // console.log(req.headers.authorization);
 
-    const username = req.params.id;
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+    const username = login;
+    console.log(username);
+
+
+    if( Object.keys(req.query).length > 0){
+        res.status(400).end();
+    }
+
+    // res.status(200).end();
+
+    //get username here somehow
 
     User.findByPk(username)
         .then(data => {
-        if (data) {
-            console.log(data);
-            res.send(data);
-        } else {
-            res.status(400).send({
-            message: `Cannot find User with id=${username}.`
-            });
-        }
-        })
+            if (data) {
+                dataToSend = {
+                    id: data.id,
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    username: data.username,
+                    account_created: data.account_created,
+                    account_updated: data.account_updated
+                }
+                // console.log("dataToSend: "+ dataToSend.id + dataToSend.username + dataToSend.account_created)
+                res.status(200).send(dataToSend);
+            } else {
+                res.status(400).send({
+                message: `Cannot find User with id=${username}.`
+                });
+            }
+            })
         .catch(err => {
-        res.status(500).send({
-            message: "Error retrieving User with id=" + username
-        });
+            res.status(500).send({
+                message: "Error retrieving User with id=" + username +" " + err
+            });
         });
 }
 
-exports.passwordAuthorizer = function (username, password, cb) {
-    if (this.passwordVerifier(username, password))
-        return cb(null, true)
-    else
-        return cb(null, false)
-}
+exports.updateuser =  async (req, res) => {
+    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.set('Content-Type', 'application/json');
+    res.set('X-Content-Type-Options', 'nosniff');
+
+    if( Object.keys(req.query).length > 0 || req.body.username == "" || req.body.username == null || req.body.username == undefined ){
+        res.status(400).send("Username cannot be empty");
+    }
+
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+    valuestoupdate = {};
+
+    //need to test out with empty obj/first_name to see if value is "" or null or undefined  - done
+    if(req.body.first_name != "" && req.body.first_name != null && req.body.first_name != undefined){
+        valuestoupdate.first_name = req.body.first_name;
+    }
+    if(req.body.last_name != "" && req.body.last_name != null && req.body.last_name != undefined){
+        valuestoupdate.last_name = req.body.last_name;
+    }
+    if(req.body.username != "" && req.body.username != null && req.body.username != undefined){
+        valuestoupdate.username = req.body.username;
+    }
+    if(req.body.password != "" && req.body.password != null && req.body.password != undefined){
+        valuestoupdate.password = req.body.password;
+        this.passwordEncrypter(valuestoupdate.password).then(
+            (encryptedPassword) => {
+                // console.log(encryptedPassword)
+
+                valuestoupdate.password = encryptedPassword;
+                valuestoupdate.account_updated = new Date();
+            
+                // console.log(user);
+            
+                User.update(valuestoupdate, {
+                    where: {username: username}
+                })
+                .then(num => {
+                    if (num == 1) {
+                        res.status(204).end();
+                    } else {
+                      res.status(400).send({
+                        message: `Cannot update User with username= ${username}`
+                      });
+                    }
+                  })
+                  .catch(err => {
+                    res.status(500).send({
+                      message: "Error updating User with username=" + username + " "+ err
+                    });
+                  });
+            },
+            (onRejected) => {
+                console.log("Error with password hashing: "+ onRejected);
+                // Some task on failure
+                res.status(500).send({
+                    message: onRejected
+                });
+            }
+        )}else{
+            //password doesn't need to be updated
+            User.update(valuestoupdate, {
+                where: {username: username}
+            })
+            .then(num => {
+                if (num == 1) {
+                    res.status(204).end();
+                } else {
+                  res.status(400).send({
+                    message: `Cannot update User with username= ${username}`
+                  });
+                }
+              })
+              .catch(err => {
+                res.status(500).send({
+                  message: "Error updating User with username=" + username + " " + err
+                });
+              });
+        }
+    }
+
+
+
+
+// exports.passwordAuthorizer = function (username, password, cb) {
+//     if (this.passwordVerifier(username, password))
+//         return cb(null, true)
+//     else
+//         return cb(null, false)
+// }
 
 exports.passwordEncrypter = async function(password) {
     try {
@@ -102,38 +212,38 @@ exports.passwordEncrypter = async function(password) {
     }
   }
 
-exports.passwordVerifier = function(username, password) {
-    //get row based on username
+// exports.passwordVerifier = function(username, password) {
+//     //get row based on username
 
-    var hash;
-    User.findByPk(username)
-    .then(data => {
-        if (data) {
-            hash = data.password;
-        } else{
-            console.log("User not found: " + username)
-            return false;
-        }
-    })
-    .catch(err => {
-        console.log("error while verfying pwd " + err)
-        return false;
-      });
+//     var hash;
+//     User.findByPk(username)
+//     .then(data => {
+//         if (data) {
+//             hash = data.password;
+//         } else{
+//             console.log("User not found: " + username)
+//             return false;
+//         }
+//     })
+//     .catch(err => {
+//         console.log("error while verfying pwd " + err)
+//         return false;
+//       });
 
-    console.log("Hash found: " + hash);
+//     console.log("Hash found: " + hash);
 
-    if(bcrypt.compare(password, hash, function(err, result) {
-        if (result) {
-            console.log("Password verified");
-            return true;
-        }
-        else {
-            console.log("Password not verified");
-            return false;
-        }
-      })){
+//     if(bcrypt.compare(password, hash, function(err, result) {
+//         if (result) {
+//             console.log("Password verified");
+//             return true;
+//         }
+//         else {
+//             console.log("Password not verified");
+//             return false;
+//         }
+//       })){
 
-      }
-  }
+//       }
+//   }
 
   
